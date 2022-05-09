ee57283181f4fda194fef6ce6188159e285cfee9
Savosin Anton <anle@list.ru>
2022-05-09 10:50:32 +0300

    Домашнее задание к занятию «3.4 Fragments, FragmentManager» - начнём помолясь.

--- app/src/main/java/ru/netology/nmedia/data/impl/FilePostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/data/impl/FilePostRepository.kt
@@ -20,8 +20,7 @@ class FilePostRepository(
 
     private val gson = Gson()
     private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
-    private val data
-    : MutableLiveData<List<Post>>
+    private val data : MutableLiveData<List<Post>>
 
     init {
         val postsFile = application.filesDir.resolve(REPOSITORY_FILE_NAME)
@@ -29,7 +28,63 @@ class FilePostRepository(
             val inputStream = application.openFileInput(REPOSITORY_FILE_NAME)
             val reader = inputStream.bufferedReader()
             reader.use { gson.fromJson(it, type) }
-        } else emptyList()
+        } else
+            listOf(
+                Post(
+                    id = 3L,
+                    author = "Нетология. Университет интернет-профессий",
+                    content = "Помогаем расти в профессии и открывать новые карьерные горизонты \uD83D\uDCAB\n" +
+                            "\n" +
+                            "Нетология — это четыре уровня образования:\n" +
+                            "\n" +
+                            "\uD83D\uDD39 Для начинающих, которые хотят освоить актуальную профессию с нуля\n" +
+                            "\uD83D\uDD39 Для специалистов, которые стремятся вырасти в карьере\n" +
+                            "\uD83D\uDD39 Для руководителей, которые нацелены усилить лидерские навыки\n" +
+                            "\uD83D\uDD39 И для компаний, которым нужно обучить команду\n" +
+                            "\n" +
+                            "Маркетинг, дизайн, программирование, soft skills, MBA, управление проектами, аналитика и Data Science — выбирайте, что вам ближе и приходите к нам за знаниями и ростом.\n" +
+                            "\n" +
+                            "Перемены — всегда правильный выбор \uD83D\uDC4C\n" +
+                            "http://netolo.gy/fEU\n" +
+                            "8 (800) 301-39-69\n" +
+                            "Варшавское шоссе, д. 1, стр. 6, 1 этаж, офис 105А, Москва",
+                    videoContent = "https://youtu.be/xOgT2qYAzds",
+                    published = "21 мая 2020",
+                    likedByMe = true,
+                    likes = 1100,
+                    sharedByMe = false,
+                    shared = 999997,
+                ),
+                Post(
+                    id = 2L,
+                    author = "Skillbox. Образовательная платформа",
+                    content = "Skillbox — образовательная платформа, которая объединяет ведущих экспертов и практиков рынка, методистов и продюсеров образовательного контента.\n" +
+                            "\n" +
+                            "Skillbox лидер сегмента дополнительного профессионального онлайн-образования согласно данным исследования “Интерфакс Академии” в 2020 году, лидер рейтинга РБК EdTech-компаний за второй квартал 2020 года, победитель премии Рунета в номинациях “Образование и кадры” (2018, 2020) и “Технологии и инновации” (2019).\n" +
+                            "https://vk.cc/9OuvQO\n" +
+                            "8 (800) 500-05-22\n" +
+                            "Обучающие курсы\n" +
+                            "Ленинский проспект, дом 6, строение 20, Москва",
+                    videoContent = "https://youtu.be/Q0KL76IK4_0",
+                    published = "1 ноя 2021",
+                    likedByMe = false,
+                    likes = 99,
+                    sharedByMe = false,
+                    shared = 997,
+                ),
+                Post(
+                    id = 1L,
+                    author = "Некая образовательная организация",
+                    content = "Тут информация об этой организации",
+                    videoContent = null,
+                    published = "ДД ммм ГГГГ",
+                    likedByMe = false,
+                    likes = 0,
+                    sharedByMe = false,
+                    shared = 0,
+                )
+            )
+//            emptyList()
         data = MutableLiveData(posts)
     }
 

9dab0c55a4ffbf43a648bec18868d091e2457a65
Савосин Антон <anle@list.ru>
2022-05-12 13:56:49 +0300

    Домашнее задание к занятию «3.4 Fragments, FragmentManager» - пост вьювинг фрагмент отображается, заполняется и скролится, но на синтетиках...

--- app/src/main/java/ru/netology/nmedia/ui/PostViewingFragment.kt
+++ app/src/main/java/ru/netology/nmedia/ui/PostViewingFragment.kt
@@ -6,20 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.post_card_layout.*
 import kotlinx.android.synthetic.main.post_card_layout.view.*
 import ru.netology.nmedia.R
+import ru.netology.nmedia.adapters.PostsAdapter
 import ru.netology.nmedia.databinding.PostEditContentFragmentBinding
 import ru.netology.nmedia.databinding.PostViewingFragmentBinding
 import ru.netology.nmedia.ui.PostEditContentFragment.Companion.REQUEST_KEY
 import ru.netology.nmedia.ui.PostEditContentFragment.Companion.RESULT_KEY
 import ru.netology.nmedia.valueToStringForShowing
+import ru.netology.nmedia.view_models.PostViewModel
 
 class PostViewingFragment : Fragment() {
 
-        private val args by navArgs<PostViewingFragmentArgs>()
+    private val viewModel by viewModels<PostViewModel>()
+    private val args by navArgs<PostViewingFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,11 +31,15 @@ class PostViewingFragment : Fragment() {
         savedInstanceState: Bundle?
     ) = PostViewingFragmentBinding.inflate(
         layoutInflater, container, false
-    ).also {
-            binding ->
-        with(binding) {
-            val postToViewing = args.postToViewing
+    )
+//        .also { binding ->
+//        val adapter = PostsAdapter(viewModel)
+//    }
+        .root
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+            val postToViewing = args.postToViewing
             post_avatar?.setImageResource(
                 when (postToViewing.author) {
                     "Нетология. Университет интернет-профессий" ->
@@ -65,13 +73,6 @@ class PostViewingFragment : Fragment() {
             post_heart?.isChecked = postToViewing.likedByMe
             share?.text = valueToStringForShowing(postToViewing.shared)
             share?.isChecked = postToViewing.sharedByMe
-}
-    }.root
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val postToViewing = args.postToViewing
-        post_author?.text = postToViewing.author
 
     }
 

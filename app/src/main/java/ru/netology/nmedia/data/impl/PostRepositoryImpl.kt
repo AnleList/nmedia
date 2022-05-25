29ae2a4a34146b1291bad6e1f853ca4e40e76523
Савосин Антон <anle@list.ru>
2022-05-25 15:37:08 +0300

    Домашнее задание к занятию «4.2 Библиотека Room» - поправил баг с вылетом при удалении поста на фрагмете просмотра поста - добавил инит пост

--- app/src/main/java/ru/netology/nmedia/data/impl/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/data/impl/PostRepositoryImpl.kt
@@ -16,6 +16,38 @@ class PostRepositoryImpl(
         entities.map { it.toModel() }
     }
 
+    init {
+        val postNetology = Post(
+            id = 3L,
+            author = "Нетология. Университет интернет-профессий",
+            textContent = "Помогаем расти в профессии и открывать новые карьерные горизонты \uD83D\uDCAB\n" +
+                    "\n" +
+                    "Нетология — это четыре уровня образования:\n" +
+                    "\n" +
+                    "\uD83D\uDD39 Для начинающих, которые хотят освоить актуальную профессию с нуля\n" +
+                    "\uD83D\uDD39 Для специалистов, которые стремятся вырасти в карьере\n" +
+                    "\uD83D\uDD39 Для руководителей, которые нацелены усилить лидерские навыки\n" +
+                    "\uD83D\uDD39 И для компаний, которым нужно обучить команду\n" +
+                    "\n" +
+                    "Маркетинг, дизайн, программирование, soft skills, MBA, управление проектами, аналитика и Data Science — выбирайте, что вам ближе и приходите к нам за знаниями и ростом.\n" +
+                    "\n" +
+                    "Перемены — всегда правильный выбор \uD83D\uDC4C\n" +
+                    "http://netolo.gy/fEU\n" +
+                    "8 (800) 301-39-69\n" +
+                    "Варшавское шоссе, д. 1, стр. 6, 1 этаж, офис 105А, Москва",
+            draftTextContent = null,
+            videoContent = "https://youtu.be/xOgT2qYAzds",
+            published = "21 мая 2020",
+            likedByMe = true,
+            likes = 1100,
+            sharedByMe = false,
+            shared = 999997,
+        )
+        if(data.value?.isEmpty() == true || data.value == null) {
+            dao.insert(postNetology.toEntity())
+        }
+    }
+
     override fun save(post: Post) {
         dao.save(post.toEntity())
     }
